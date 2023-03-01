d9fef21324ef1928e9801612a71c403031bf3b1a
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-01 14:22:28 -0500

    added engineer html to generate file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -10,11 +10,31 @@ const generateManager = function (manager) {
                     <div class="card-body">
                         <ul class="list-group">
                             <li class="list-group-item">ID: ${manager.id} </li>
-                            <li class="list-group-item">Email: <a href="mailto:${manager.email}"> ${manager.email}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${manager.email}"> ${manager.email}</a></li>
                             <li class="list-group-item">Office #: ${manager.officeNumber} </li>
                         </ul>
                     </div>
                 </div>        
             </div>
             `;
-}
+};
+
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+                <div class="card h-100">
+                    <div class="card-header bg-info">
+                        <h3>${engineer.name}</h3>
+                        <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${engineer.id}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `;
+};
