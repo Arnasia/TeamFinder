c51278491dd859eaeff8c1b65c81accfbf31403b
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-01 14:26:14 -0500

    added intern html to generate file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -38,3 +38,23 @@ const generateEngineer = function (engineer) {
             </div>
             `;
 };
+
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+                <div class="card h-100">
+                    <div class="card-header bg-info">
+                        <h3>${intern.name}</h3>
+                        <h4>Intern</h4><i class="fas fa-user-graduate mr-2"></i>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${intern.id}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                            <li class="list-group-item">School: ${intern.school}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>         
+    `;
+};
