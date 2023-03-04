af73b857d174c0a49ec65aa355182140dc057a41
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-03 19:29:07 -0500

    added the option to chose which member to add next and then prompted with series of question about the new member

--- index.js
+++ index.js
@@ -53,3 +53,70 @@ const createManager = () => {
         console.log(manager); 
     })
 };
+
+const addMember = () => {
+    console.log(`Let's Add To The Team!!`);
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose your employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What's the name of the employee?", 
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the employee's ID.",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the employee's email.",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the employee's github username.",
+            when: (input) => input.role === "Engineer",
+            validate: github => {
+                if (github) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's github username!")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: school => {
+                if (school) {
+                    return true;
+                } else {
+                    console.log ("Please enter the intern's school!")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddMember',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
